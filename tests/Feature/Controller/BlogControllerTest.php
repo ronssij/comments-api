26b065cef505811323989400f4d0734a9a99fa5b
CJ Ronxel <cj@davidvenneit.com>
2021-10-30 12:07:43 +0800

    add blog list unit testing

---
+++ tests/Feature/Controller/BlogControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Controller;
+
+use Tests\TestCase;
+use App\Models\Blog;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BlogControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->blog = factory(Blog::class, 100)->create();
+    }
+
+    /** @test */
+    public function userShouldSeeListOfBlogs()
+    {
+        $response = $this->getJson(route('blogs'))
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    ['title', 'body', 'comments', 'created_at']
+                ]
+            ]);
+
+        $this->assertCount(100, $response->json()['data']);
+    }
+}
